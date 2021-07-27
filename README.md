b9af87bc6f3df34ba6f4000ec34013fc2421fb89
rahafalbakkar <rahaf.a7med1991@gmail.com>
2021-07-27 17:11:22 +0300

    add new question for user with chance to solve it

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # guessing-game
+________
+ ## project by **Rahaf ALBAKKAR**
+
+ *this is code-201 that allowed me to learn JS, CSS and HTML*
+ ____
+ ###
+ A bout my project I try to make it like game and puzzel I think that best practise to learn how to apply all of topic that I git it from journy.
+
+ __
+ 27.JULY.21: add LOOP and some of condition "applied what I learend in class"
+    1.I add new questions for user and give him opportunities to solve it and give him feedback with correct anwer. 
+    
--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 
 'use strict';
 
+let counter = 0;
 
 let helloMsg = prompt('Welcom to guessing-game. Colud you write your name please!!');
 
@@ -8,50 +9,101 @@ let message = alert('Before we get start the user name is:' + helloMsg);
 let play = confirm('do you enjoy to play numirce puzzel!!');
 console.log(play);
 
-let subtract = prompt ('does Subtract 869 from 2482 equal 1613').toLocaleLowerCase();
+let subtract = prompt('does Subtract 869 from 2482 equal 1613').toLocaleLowerCase();
 
 switch (subtract) {
   case 'yes':
+  case 'y':
     alert('that is right, let move to the snd question');
     console.log(subtract)
+    counter++;
     break;
 
   case 'no':
-   alert('try another question');
-  
+  case 'n':
+    alert('try another question');
+
 }
 
-let divided = prompt ('is divided 1000 by 16 equal 50.5').toLocaleLowerCase();
-if(divided = 'no'){
+let divided = prompt('is divided 1000 by 16 equal 50.5').toLocaleLowerCase();
+/*if (divided = 'no') {
   alert('you are good in math so let take another question');
-  console.log(divided);
+  counter++;
+  console.log(divided); 
 }
-else
-{ alert('unfortuntely,that is wrong');
+else {
+  alert('unfortuntely,that is wrong');
+break;
+}*/
+switch (divided) {
+  case 'no':
+  case 'n':
+    alert('you are good in math so let take another question');
+    console.log(divided)
+    counter++;
+    break;
 
+  case 'yes':
+  case 'y':
+    alert('unfortuntely,that is wrong');
 }
 
 let sequnce = prompt('to complete this sequnce 1, 1, 2, 3, X , 8, 13 the value of X should 5 ').toLocaleLowerCase();
-switch(sequnce){
+switch (sequnce) {
   case 'yes':
+  case 'y':
     alert('an awseom this hard question but you solve it..lets go')
     console.log(sequnce);
+   counter++;
     break;
-  
-  case 'no':
-    alert('Dont worry,try last one');
 
-    break;
+  case 'no':
+  case 'n':
+    alert('Dont worry,try last one');
+break;
 
 }
 
-let average =prompt('the mean average of 3, 15, 8 and 22 is 14 ').toLocaleLowerCase();
-if(average = 'no'){
+let average = prompt('the mean average of 3, 15, 8 and 22 is 14 ').toLocaleLowerCase();
+/*if (average = 'no') {
   alert('very very good');
   console.log(average);
-} else
-{
-  alert('Good luck');
-} 
+  counter++;
 
-alert('Thanks for your visit' + helloMsg);
+} else if (average = 'yes') {
+  alert('Good luck');
+  break;
+}*/
+switch (average) {
+  case 'no':
+  case 'n':
+    alert('very very good')
+    console.log(sequnce);
+   counter++;
+    break;
+
+  case 'yes':
+  case 'y':
+    alert('Good Luck');
+break;
+
+}
+alert('Thanks for your visit' + helloMsg + 'Now!! after you answer numeric puzzel let us check Your knowledge of geography');
+//the nile river/Length 6,650 km
+for (let i = 0; i < 4; i++) {
+  let lengthRiver = prompt('the Nile river/Length is :');
+  if (lengthRiver < 6650) { alert('too low') }
+
+  else if (lengthRiver > 6650) { alert('too high') }
+
+  else {
+    if (lengthRiver === 6650) {
+      alert('this is right answer ^_^')
+      counter++;
+    }
+   
+    break;
+  }
+
+}
+alert('your dgree is' + counter)
