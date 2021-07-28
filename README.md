2b3ad227b5d902c2a794dddd1b413b07b8c9e3d7
rahafalbakkar <rahaf.a7med1991@gmail.com>
2021-07-28 16:36:20 +0300

    craet new styel

--- README.md
+++ README.md
@@ -9,5 +9,16 @@ ________
 
  __
  27.JULY.21: add LOOP and some of condition "applied what I learend in class"
-    1.I add new questions for user and give him opportunities to solve it and give him feedback with correct anwer. 
+    1.I add new questions for user and give him opportunities to solve it and give him feedback with correct anwer.
+_____
+28.July.21
+- Today we started work in pairs 
+I worked with my partnet and divided time between other to duscuis what we wel do 
+this is skill to learn "Time management and team work". 
+
+-the names of the Driver: Mohmammad  
+ Navigator: me then we switch the role.
+
+- Today we learned about functions we definded the mean of function and knew where and how to use it.
+
     
--- app/app.js
+++ app/app.js
@@ -8,127 +8,142 @@ let helloMsg = prompt('Welcom to guessing-game. Colud you write your name please
 let message = alert('Before we get start the user name is: ' + helloMsg);
 let play = confirm('do you enjoy to play numerical puzzel!!');
 console.log(play);
+function subtractMath() {
+  let subtract = prompt('does Subtract 869 from 2482 equal 1613').toLocaleLowerCase();
 
-let subtract = prompt('does Subtract 869 from 2482 equal 1613').toLocaleLowerCase();
+  switch (subtract) {
+    case 'yes':
+    case 'y':
+      alert('that is right, let move to the snd question');
+      console.log(subtract)
+      counter++;
+      break;
 
-switch (subtract) {
-  case 'yes':
-  case 'y':
-    alert('that is right, let move to the snd question');
-    console.log(subtract)
-    counter++;
-    break;
+    case 'no':
+    case 'n':
+      alert('try another question');
 
-  case 'no':
-  case 'n':
-    alert('try another question');
-
+  }
 }
+subtractMath();
 
-let divided = prompt('is divided 1000 by 16 equal 50.5').toLocaleLowerCase();
-/*if (divided = 'no') {
-  alert('you are good in math so let take another question');
-  counter++;
-  console.log(divided); 
-}
-else {
-  alert('unfortuntely,that is wrong');
-break;
-}*/
-switch (divided) {
-  case 'no':
-  case 'n':
-    alert('you are good in math so let take other question');
-    console.log(divided)
+function dividedMath() {
+  let divided = prompt('is divided 1000 by 16 equal 50.5').toLocaleLowerCase();
+  /*if (divided = 'no') {
+    alert('you are good in math so let take another question');
     counter++;
-    break;
-
-  case 'yes':
-  case 'y':
+    console.log(divided); 
+  }
+  else {
     alert('unfortuntely,that is wrong');
-}
-
-let sequnce = prompt('to complete this sequnce 1, 1, 2, 3, X , 8, 13 the value of X should 5 ').toLocaleLowerCase();
-switch (sequnce) {
-  case 'yes':
-  case 'y':
-    alert('An awesome this hard question but you solve it..lets go')
-    console.log(sequnce);
-    counter++;
-    break;
-
-  case 'no':
-  case 'n':
-    alert('Dont worry,try last one');
-    break;
-
-}
-
-let average = prompt('the mean average of 3, 15, 8 and 22 is 14 ').toLocaleLowerCase();
-/*if (average = 'no') {
-  alert('very very good');
-  console.log(average);
-  counter++;
-
-} else if (average = 'yes') {
-  alert('Good luck');
   break;
-}*/
-switch (average) {
-  case 'no':
-  case 'n':
-    alert('very very good')
+  }*/
+  switch (divided) {
+    case 'no':
+    case 'n':
+      alert('you are good in math so let take other question');
+      console.log(divided)
+      counter++;
+      break;
+
+    case 'yes':
+    case 'y':
+      alert('unfortuntely,that is wrong');
+  }
+}
+dividedMath();
+
+function sequnceMath() {
+  let sequnce = prompt('to complete this sequnce 1, 1, 2, 3, X , 8, 13 the value of X should 5 ').toLocaleLowerCase();
+  switch (sequnce) {
+    case 'yes':
+    case 'y':
+      alert('An awesome this hard question but you solve it..lets go')
+      console.log(sequnce);
+      counter++;
+      break;
+
+    case 'no':
+    case 'n':
+      alert('Dont worry,try last one');
+      break;
+
+  }
+}
+sequnceMath();
+
+function averageMath() {
+  let average = prompt('the mean average of 3, 15, 8 and 22 is 14 ').toLocaleLowerCase();
+  /*if (average = 'no') {
+    alert('very very good');
     console.log(average);
     counter++;
+  
+  } else if (average = 'yes') {
+    alert('Good luck');
     break;
+  }*/
+  switch (average) {
+    case 'no':
+    case 'n':
+      alert('very very good')
+      console.log(average);
+      counter++;
+      break;
 
-  case 'yes':
-  case 'y':
-    alert('Good Luck');
-    break;
+    case 'yes':
+    case 'y':
+      alert('Good Luck');
+      break;
 
+  }
 }
+averageMath();
+
 alert('Thanks for your visit ' + helloMsg + ' Now!! after you answer numerical puzzel let us check Your knowledge of geography');
 
-//the nile river/Length 6,650 km
-for (let i = 0; i < 4; i++) {
-  let lengthRiver = prompt('the Nile river/Length is :');
-  console.log(lengthRiver);
-  if (lengthRiver < 6650) { alert('too low') }
+function nileLength() {
+  //the nile river/Length 6,650 km
+  for (let i = 0; i < 4; i++) {
+    let lengthRiver = prompt('the Nile river/Length is :');
+    console.log(lengthRiver);
+    if (lengthRiver < 6650) { alert('too low') }
 
-  else if (lengthRiver > 6650) { alert('too high') }
+    else if (lengthRiver > 6650) { alert('too high') }
 
-  else {
-    if (lengthRiver == 6650) {
-      alert('this is right answer ^_^' + 'The Nile River Lenght ' + lengthRiver)
-      counter++;
+    else {
+      if (lengthRiver == 6650) {
+        alert('this is right answer ^_^' + 'The Nile River Lenght ' + lengthRiver)
+        counter++;
+      }
+      break;
     }
-    break;
-  }
-
-}
-alert('The Nile River lenght is 6650 km')
-
-//
-let longestRivers = ['Nile River', 'Amazone River', 'Yangtze River', 'Mississippi', 'Yenisei'];
-for (let i = 0; i < 6; i++) {
-  let river = prompt('can you guess What is the longest rivers in the world !!');
-  console.log(river);
-
-  for (let x; i < longestRivers.length; x++) {
-    if (river = longestRivers[i]) { alert('that is far, try again') }
-    else if (river == longestRivers[i]) {
-      alert('this correct answer one og hte longest river in the world is ' + longestRiver[i])
-
-    }
-    counter++;
-    break;
 
   }
+  alert('The Nile River lenght is 6650 km')
 }
+nileLength();
+
+function theLongest() {
+  let longestRivers = ['Nile', 'Amazone', 'Yangtze', 'Mississippi', 'Yenisei'];
+  for (let i = 0; i < 6; i++) {
+    let river = prompt('can you guess What is the longest rivers in the world !!').toLocaleLowerCase();
+    console.log(river);
+
+    for (let x; i < longestRivers.length; x++) {
+      if (river = longestRivers[i]) { alert('that is far, try again') }
+      else if (river == longestRivers[i]) {
+        alert('this correct answer one og hte longest river in the world is ' + longestRiver[i])
+        counter++;
+      }
+      break;
+    }
+  }
+}
+theLongest();
 
 console.log(counter);
-alert('The five longest rivers in the wolrd are Nile ,Amazon, Yangtze,Mississippiand Yenisei')
+alert('The five longest rivers in the wolrd are Nile ,Amazon, Yangtze,Mississippi and Yenisei')
 alert('your Result is ' + counter);
 
 let longestRiver = ["Nile", "Amazon", "Mississippi", "Yenisei"];
